f89d22bd3ad0a903ecc51af9feab0b632bdfb45b
Leticia <let.rostirola@gmail.com>
2016-07-12 21:41:21 -0300

    add model carrinho

--- myecommerce/carrinho/models.py
+++ myecommerce/carrinho/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=256)
+	description = models.TextField()
+	price  = models.DecimalField(max_digits=7,decimal_places=2)
+	photo = models.ImageField(blank=True)
+	active = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
+
+	def __repr__(self):
+		return 'Product({!r}, active={!r})'.format(self.name, self.active)
